af6a33964b44a649bb1860b94d1628644de1b026
MatchaOnMuffins <henryzhangjcle@gmail.com>
2022-07-01 18:58:34 -0400

    Added option to choose between SFW and NSFW images.

--- bitches/__init__.py
+++ bitches/__init__.py
@@ -4,28 +4,34 @@ from os import mkdir, sep
 from random import randint
 from threading import Thread
 
-api = 'https://api.waifu.pics/nsfw/waifu'
+sfw_api = 'https://api.waifu.pics/sfw/waifu'
+nsfw_api = 'https://api.waifu.pics/nsfw/waifu'
 
 
-def get(directory="bitches", amount=randint(5, 10)):
-    '''can choose directory name too'''
+def get(directory="bitches", amount=randint(5, 10), nsfw=False):
+    """can choose directory name too"""
     try:
         mkdir(directory)
-    except Exception:
+    except Exception as e:
+        print(e)
         pass
     for i in range(amount):
-        Thread(target=bitches, args=(directory, amount)).start()
+        Thread(target=bitches, args=(directory, amount, nsfw)).start()
 
 
-def bitches(dir_, amount=randint(3, 7)):
+def bitches(dir_, amount=randint(3, 7), nsfw=False):
     for i in range(amount):
-        req_url = requests.get(api)
+        if nsfw:
+            req_url = requests.get(nsfw_api)
+        else:
+            req_url = requests.get(sfw_api)
         url = req_url.json()['url']
         if not req_url.ok:
-            print("error: "+req_url)
-        with open(dir_+sep+url[21:], 'wb') as f:
+            print("error: " + str(req_url))
+        with open(dir_ + sep + url[21:], 'wb') as f:
             response = requests.get(url, stream=True)
             for block in response.iter_content(1024):
                 if not block:
                     break
                 f.write(block)
+
